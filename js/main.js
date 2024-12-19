79bd082fbddb3374e4686c054cf84f97fdbaab03
EduardaLinux <ifeduarda.luiz@gmail.com>
2024-12-18 23:43:23 -0300

    Primeiro Commit

--- js/main.js
+++ js/main.js
@@ -1,89 +1,74 @@
-const hoverCard = document.querySelector('#card-test')
-const hoverText = hoverCard.querySelector('#card-test h3')
-const style = document.querySelector('style')
-const hoverTextPath = "#tests #card-test h3"
-const hoverCardPath = "#tests #card-test"
+const cardList = document.getElementById('card-list')
+const hour = document.querySelector('#hours')
+const min = document.querySelector('#min')
+const sec = document.querySelector('#sec')
+const milis = document.querySelector('#milis')
+const btns = document.querySelector('#start')
+const btnsOn = document.querySelector('#on')
+const startBtn = document.querySelector("#startBtn")
 
-const transitions = (transition) => {
-    let cardColor = document.getElementById('cardColor').value
-    let textColor = document.getElementById('textColor').value
-    const delay = document.getElementById('delay').value
-    if(cardColor === ''){ cardColor = 'rgba(0, 0, 0, 0.479)' }
-    if(textColor === ''){ textColor = 'white' }
+let enumeracao = 1
+let intervalo
+let hours = 0
+let minutes = 0
+let seconds = 0
+let miliseconds = 0
+
+const start = () =>{
+    btns.style.display = 'none'
+    btnsOn.style.display = 'flex'
+    intervalo = setInterval(()=>{
+        miliseconds += 10
+        if(miliseconds === 1000){
+            miliseconds = 0
+            seconds++
+        }
+        if(seconds === 60){
+            seconds = 0
+            minutes++
+        }
+        if(minutes === 60){
+            minutes = 0
+            hours++
+        }
+        hour.innerText = (hours<10) ? `0${hours}` : hours
+        min.innerText = (minutes<10) ? `0${minutes}` : minutes
+        sec.innerText = (seconds<10) ? `0${seconds}` : seconds
+        milis.innerText = (miliseconds<100) ? `${miliseconds}`.padStart(3, '0') : miliseconds
+    }, 10)
     
-    if(transition === null){
-        style.textContent = `
-            ${hoverCardPath}:after{
-                bottom: 0;
-                left: 0;
-            }
-            ${hoverCardPath}:hover:after{
-                width: 100%;
-                background-color: ${cardColor};
-                
-            }
-            ${hoverCardPath}:hover{
-                color: ${textColor};
-            }
-        `
-    } 
-    else if(transition.startsWith('cubic')){
-        style.textContent = `
-        ${hoverCardPath}:after{
-            bottom: 0;
-            left: 0;
-        }
-        ${hoverCardPath}:hover:after{
-            width: 100%;
-            background-color: ${cardColor};
-            transition: ${transition}(0, 0.81, 0.5, .97) ${delay}s;
-        }
-        ${hoverCardPath}:hover{
-            color: ${textColor};
-        }
-    ` 
-    }
-    else{
-        style.textContent = `
-            ${hoverCardPath}:after{
-                bottom: 0;
-                left: 0;
-            }
-            ${hoverCardPath}:hover:after{
-                width: 100%;
-                background-color: ${cardColor};
-                transition: ${transition} ${delay}s;
-            }
-            ${hoverCardPath}:hover{
-                color: ${textColor};
-            }
-        `
-    }
 }
-const transform = (type) => {
-    const value = document.getElementById('number').value
-    if(type.startsWith('rotate') || type.startsWith('skew')){
-        style.textContent = `
-                ${hoverTextPath}{
-                    transform: ${type}(${value}deg);
-                }
-            `
-    }
-    else if(type.startsWith('scale')){
-        style.textContent = `
-                ${hoverTextPath}{
-                    transform: ${type}(${value});
-                }
-            `
-            setTimeout(()=>{
-                style.textContent= ''
-            },5000)
-    }
-    else{
-        style.textContent = `
-        ${hoverTextPath}{
-            transform: ${type}(${value}px);
-        }
+const pause = ( ) =>{
+    clearInterval(intervalo)
+    btnsOn.style.display = 'none'
+    btns.style.display = 'flex'
+}
+const note = ( ) =>{
+    cardList.innerHTML += `
+    <div class="card">
+                <header>
+                    <p>${enumeracao}</p>
+                </header>
+                <main>
+                    <h4 id="card-hour">${hour.innerText}</h4>
+                    <p id="card-dots">:</p>
+                    <h4 id="card-min">${min.innerText}</h4>
+                    <h4 id="card-sec">${sec.innerText}</h4>
+                </main>
+            </div>
     `
-    }
+    enumeracao++;
+}
+const reset = ( ) =>{
+    cardList.innerHTML = ' '
+    hour.innerText = '00'
+    min.innerText ='00'
+    sec.innerText = '00'
+    milis.innerText = '000'
+    intervalo
+    hours = 0
+    minutes = 0
+    seconds = 0
+    miliseconds = 0
+    enumeracao= 1
 }
